13f59c95b2e3f4e1a4ccf261c1434047d3f7d12c
LuciusEverest <61430189+LuciusEverest@users.noreply.github.com>
2021-07-28 09:15:15 -0600

    Scene Fixes

--- Assets/Scripts/FlickerLights.cs
+++ Assets/Scripts/FlickerLights.cs
@@ -19,10 +19,10 @@ public class FlickerLights : MonoBehaviour
     {
         isFlickering = true;
         this.gameObject.GetComponent<Light>().enabled = false;
-        timeDelay = Random.Range(0.01f, 0.3f);
+        timeDelay = Random.Range(0.01f, 0.5f);
         yield return new WaitForSeconds(timeDelay);
         this.gameObject.GetComponent<Light>().enabled = true;
-        timeDelay = Random.Range(0.01f, 0.3f);
+        timeDelay = Random.Range(0.01f, 0.5f);
         yield return new WaitForSeconds(timeDelay);
         isFlickering = false;
     }
